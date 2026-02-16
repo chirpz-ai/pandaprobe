8f3f1e9a45bc4da6649890a345e18a8288b54c34
sinatayebati <sinatayebati@gmail.com>
2026-02-16 00:38:05 -0600

    updating api routes

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -2,10 +2,11 @@
 
 from fastapi import APIRouter
 
-from app.api.v1.routes import health, organizations, traces
+from app.api.v1.routes import evaluations, health, organizations, traces
 
 v1_router = APIRouter()
 
 v1_router.include_router(health.router)
 v1_router.include_router(organizations.router)
 v1_router.include_router(traces.router)
+v1_router.include_router(evaluations.router)
