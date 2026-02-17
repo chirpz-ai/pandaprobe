cfa28d84ae69872729d4e8de36356780ee04f310
sinatayebati <sinatayebati@gmail.com>
2026-02-17 12:36:06 -0600

    removing integrations - it will be handled by sdk layer

--- app/integrations/langchain/__init__.py
+++
@@ -1,5 +0,0 @@
-"""LangChain integration — trace normalisation for LangChain callbacks."""
-
-from app.integrations.langchain.transformer import LangChainTransformer
-
-__all__ = ["LangChainTransformer"]
