dc184c0c72961b4d9666148b4babbe270b5fb5c1
sinatayebati <sinatayebati@gmail.com>
2026-04-18 00:51:57 -0500

    wrapping layout with polling tracker

--- frontend/src/app/org/[orgId]/project/[projectId]/layout.tsx
+++ frontend/src/app/org/[orgId]/project/[projectId]/layout.tsx
@@ -1,11 +1,16 @@
 "use client";
 
 import { ProjectProvider } from "@/components/providers/ProjectProvider";
+import { EvalRunTrackerProvider } from "@/components/providers/EvalRunTrackerProvider";
 
 export default function ProjectLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <ProjectProvider>{children}</ProjectProvider>;
+  return (
+    <ProjectProvider>
+      <EvalRunTrackerProvider>{children}</EvalRunTrackerProvider>
+    </ProjectProvider>
+  );
 }
